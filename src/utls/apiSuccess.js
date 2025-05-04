317baa1a0c4f84fcf75f0749d847917046898ac1
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-04 23:16:13 +0600

    feat: add static methods for success responses in ApiSuccess class

--- src/utls/apiSuccess.js
+++ src/utls/apiSuccess.js
@@ -1,10 +1,28 @@
-class ApiSucess {
+class ApiSuccess {
   constructor(statusCode, message, data) {
     this.success = statusCode < 400;
     this.statusCode = statusCode;
     this.message = message;
     this.data = data ?? {};
   }
+  static success(message, data) {
+    return new ApiSucess(200, message, data);
+  }
+  static created(message, data) {
+    return new ApiSucess(201, message, data);
+  }
+  static accepted(message, data) {
+    return new ApiSucess(202, message, data);
+  }
+  static noContent(message, data) {
+    return new ApiSucess(204, message, data);
+  }
+  static resetContent(message, data) {
+    return new ApiSucess(205, message, data);
+  }
+  static customSuccess(statusCode, message, data) {
+    return new ApiSucess(statusCode, message, data);
+  }
 }
 
-export default ApiSucess;
+export default ApiSuccess;
