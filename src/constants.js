94290d166a78a96cfb7f0358bea6e5d365ed9161
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-05 02:22:50 +0600

    feat: add mail utility for sending emails with verification and password reset functionality

--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,9 @@ dotenv.config({
 const PORT = process.env.PORT ?? 8000;
 const WHITELIST_DOMAINS = process.env.WHITELIST_DOMAINS ?? 'http://localhost:8000';
 const MONGODB_URI = process.env.MONGODB_URI;
-export { MONGODB_URI, PORT, WHITELIST_DOMAINS };
+const MAIL_HOST = process.env.MAIL_HOST;
+const MAIL_PORT = process.env.MAIL_PORT;
+const MAIL_USERNAME = process.env.MAIL_USERNAME;
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+
+export { MAIL_HOST, MAIL_PASSWORD, MAIL_PORT, MAIL_USERNAME, MONGODB_URI, PORT, WHITELIST_DOMAINS };
---
+++ src/utils/mail.js
@@ -0,0 +1,77 @@
+import Mailgen from 'mailgen';
+import nodemailer from 'nodemailer';
+import { MAIL_HOST, MAIL_PASSWORD, MAIL_PORT, MAIL_USERNAME } from '../constants.js';
+const sendMail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+      name: 'Pic Labs',
+      link: 'https://mailgen.js/',
+    },
+  });
+
+  const emailBody = mailGenerator.generate(options.mailTemplate);
+  const emailText = mailGenerator.generatePlaintext(options.mailTemplate);
+
+  const transporter = nodemailer.createTransport({
+    host: MAIL_HOST,
+    port: MAIL_PORT,
+    secure: false,
+    auth: {
+      user: MAIL_USERNAME,
+      pass: MAIL_PASSWORD,
+    },
+  });
+  async function main() {
+    const info = await transporter.sendMail({
+      from: '"PIC LABS" <pic_labs@pic_labs>',
+      to: options.email,
+      subject: options.subject,
+      text: emailBody,
+      html: emailText,
+    });
+  }
+  try {
+    await main();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const verificationMail = (token) => {
+  return {
+    body: {
+      name: 'Pic Labs',
+      intro: "Welcome to Pic Labs! We're very excited to have you on board.",
+      action: {
+        instructions: 'To get started with Pic Labs, please click here:',
+        button: {
+          color: '#22BC66',
+          text: 'Confirm your account',
+          link: `${process.env.WHITELIST_DOMAINS}/api/v1/users/verify/${token}`,
+        },
+      },
+      outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+const resetPasswordMail = (otp) => {
+  return {
+    body: {
+      name: 'Pic Labs',
+      intro: 'You have requested to reset your password',
+      action: {
+        instructions: 'To reset your password, please click here:',
+        button: {
+          color: '#22BC66',
+          text: 'Reset Password',
+          link: `${process.env.WHITELIST_DOMAINS}/api/v1/users/reset-password/${otp}`,
+        },
+      },
+      outro: "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export { resetPasswordMail, sendMail, verificationMail };
