3444d35195b9e68ec644e94f118609da15cb79b7
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-04 23:13:55 +0600

    feat: extend ApiError class with additional static methods for error handling

--- src/utls/apiError.js
+++ src/utls/apiError.js
@@ -11,6 +11,24 @@ class ApiError extends Error {
       Error.captureStackTrace(this, this.constructor);
     }
   }
+  static badRequest(message = 'bad request', errors = []) {
+    return new ApiError(400, message, errors);
+  }
+  static forbidden(message = 'forbidden', errors = []) {
+    return new ApiError(403, message, errors);
+  }
+  static serverError(message = 'internal server error', errors = []) {
+    return new ApiError(500, message, errors);
+  }
+  static unauthorized(message = 'unauthorized', errors = []) {
+    return new ApiError(401, message, errors);
+  }
+  static notFound(message = 'not found', errors = []) {
+    return new ApiError(404, message, errors);
+  }
+  static customError(statusCode, message, errors = []) {
+    return new ApiError(statusCode, message, errors);
+  }
 }
 
 export default ApiError;
