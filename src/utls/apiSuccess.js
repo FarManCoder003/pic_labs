0074b8bef8b8b07ef0acf90f6539b13a64d9d747
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-04 23:16:49 +0600

    fix: correct spelling of ApiSuccess in static methods

--- src/utls/apiSuccess.js
+++ src/utls/apiSuccess.js
@@ -6,22 +6,22 @@ class ApiSuccess {
     this.data = data ?? {};
   }
   static success(message, data) {
-    return new ApiSucess(200, message, data);
+    return new ApiSuccess(200, message, data);
   }
   static created(message, data) {
-    return new ApiSucess(201, message, data);
+    return new ApiSuccess(201, message, data);
   }
   static accepted(message, data) {
-    return new ApiSucess(202, message, data);
+    return new ApiSuccess(202, message, data);
   }
   static noContent(message, data) {
-    return new ApiSucess(204, message, data);
+    return new ApiSuccess(204, message, data);
   }
   static resetContent(message, data) {
-    return new ApiSucess(205, message, data);
+    return new ApiSuccess(205, message, data);
   }
   static customSuccess(statusCode, message, data) {
-    return new ApiSucess(statusCode, message, data);
+    return new ApiSuccess(statusCode, message, data);
   }
 }
 
