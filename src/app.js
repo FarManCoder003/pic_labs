3d7043c8f32b699447c1ff34daa21385bffb16e8
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-04 23:22:38 +0600

    feat: add error handler middleware for centralized error management

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import e from 'express';
 import { WHITELIST_DOMAINS } from './constants.js';
+import errorHandler from './middlewares/errorhandler.middleware.js';
 const app = e();
 app.use(e.json());
 app.use(e.urlencoded({ extended: true }));
@@ -13,4 +14,5 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(errorHandler)
 export { app };
---
+++ src/middlewares/errorhandler.middleware.js
@@ -0,0 +1,8 @@
+import ApiError from '../utls/apiError.js';
+
+const errorHandler = (err, req, res, next) => {
+  res
+    .status(err.statusCode || 500)
+    .json(ApiError.customError(err.statusCode, err.message, err.errors));
+};
+export default errorHandler;
