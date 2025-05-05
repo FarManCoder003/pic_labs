8b657d6c73585e861e1e0851b90171b3189f5b06
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-05 22:05:38 +0600

    feat: implement user authentication methods with password hashing and JWT token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,14 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import mongoose, { Schema } from 'mongoose';
+import {
+  ACCESS_TOKEN_EXPIRES,
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_EXPIRES,
+  REFRESH_TOKEN_KEY,
+  VERIFICATION_TOKEN_EXPIRES,
+  VERIFICATION_TOKEN_KEY,
+} from '../constants.js';
 
 const userSchema = new Schema(
   {
@@ -46,4 +56,32 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || !this.password) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign({ id: this._id, email: this.email }, ACCESS_TOKEN_KEY, {
+    expiresIn: ACCESS_TOKEN_EXPIRES,
+  });
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign({ id: this._id, email: this.email }, REFRESH_TOKEN_KEY, {
+    expiresIn: REFRESH_TOKEN_EXPIRES,
+  });
+};
+
+userSchema.methods.generateVerificationToken = function () {
+  return jwt.sign({ id: this._id, email: this.email }, VERIFICATION_TOKEN_KEY, {
+    expiresIn: VERIFICATION_TOKEN_EXPIRES,
+  });
+};
+
 export const User = mongoose.models.User || mongoose.model('User', userSchema);
