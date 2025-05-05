ea08c2e3ae9ea37aa0245767e3bea3e99dc47be7
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-05 17:50:17 +0600

    feat: implement user signup functionality and fix asyncHandler parameter order

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,10 @@
+import { User } from '../models/user.model.js';
+import ApiSuccess from '../utils/apiSuccess.js';
 import asyncHandler from '../utils/asyncHandler.js';
 
 const signup = asyncHandler(async (req, res) => {
-  res.send('signup');
+  const user = await User.create(req.body);
+  res.status(201).json(ApiSuccess.success('User created successfully', user));
 });
 
 export { signup };
