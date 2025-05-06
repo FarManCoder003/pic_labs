393c8739dba5521d86279b77537ffdd643cc95dc
FarmanCoder003 <codingfarman01@gmail.com>
2025-05-06 20:28:10 +0600

    feat: add file upload handling and configure storage settings

---
+++ src/middlewares/fileUpload.middleware.js
@@ -0,0 +1,14 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/temp');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const fileUpload = multer({ storage: storage, limits: { fileSize: 5 * 1024 * 1024 } });
+export default fileUpload;
